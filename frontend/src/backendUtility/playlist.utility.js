768e76e31570f36c36668575359f7adadea28bf1
Gourav Sharma <125105311+gouravsharma1711@users.noreply.github.com>
2025-08-06 22:25:12 +0530

    Update playlist.utility.js

--- frontend/src/backendUtility/playlist.utility.js
+++ frontend/src/backendUtility/playlist.utility.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance=axios.create({
-    baseURL:"http://localhost:8000/api/v1/playlist",
+    baseURL:`${import.meta.env.VITE_API_BASE_URL}/api/v1/playlist`,
     withCredentials:true
 });
 
