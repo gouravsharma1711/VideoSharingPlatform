f577e0390db0ec1cee0cf80047fee445f83d7bab
Gourav Sharma <125105311+gouravsharma1711@users.noreply.github.com>
2025-08-06 22:53:42 +0530

    Update App.js

--- backend/src/App.js
+++ backend/src/App.js
@@ -7,6 +7,7 @@ const DataLimit="20kb";
 
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
+    methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE'],
     credentials: true
 }))
 app.use(express.urlencoded({
