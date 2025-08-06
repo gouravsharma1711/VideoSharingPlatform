57a1f9f202b3ffd78567554c47fc06f2b150154e
Gourav Sharma <125105311+gouravsharma1711@users.noreply.github.com>
2025-08-06 22:23:43 +0530

    Update comments.utility.js

--- frontend/src/backendUtility/comments.utility.js
+++ frontend/src/backendUtility/comments.utility.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api/v1/comments",
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1/comments`,
     withCredentials: true
 });
 class Comments{
