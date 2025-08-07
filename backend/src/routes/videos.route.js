2af6c960886c68ca7fb4c4329e6925829c81eba6
Gourav Sharma <125105311+gouravsharma1711@users.noreply.github.com>
2025-08-07 20:24:45 +0530

    Update videos.route.js

--- backend/src/routes/videos.route.js
+++ backend/src/routes/videos.route.js
@@ -18,20 +18,25 @@ import auth from '../middlewares/auth.middleware.js';
 import { upload } from '../middlewares/multer.middleware.js';
 
 
-router.route('/:userId/videos').get(getUserVideos);
+// Get all videos
 router.route('/').get(getAllVideos);
-router.route('/user-videos').get(auth,getCurrentUserVideos);// add this in postman to test it
-router.route('/publish-video').post(upload.fields([
-    {name:"videoFile",maxCount:1},
-    {name:"thumbnail",maxCount:1}
-]),auth,publishVideo);
-
-router.route('/video/:id').get(getVideoById);
-router.route('/update/:videoId').patch(upload.none(),auth,updateVideo);
-router.route('/delete/:videoId').delete(auth,deleteVideo);
-router.route("/publish-status/:videoId").get(auth,togglePublishStatus);
-router.route('/update-thumbnail/:videoId').patch(upload.single("thumbnail"),auth,updateThumbnail);
+router.route('/user-videos').get(auth, getCurrentUserVideos); // add this in postman to test it
+router.route('/:userId/videos').get(getUserVideos);
 router.route('/:videoId').get(getSingleView);
+router.route('/video/:id').get(getVideoById);
+router.route('/publish-video').post(
+  upload.fields([
+    { name: "videoFile", maxCount: 1 },
+    { name: "thumbnail", maxCount: 1 }
+  ]),
+  auth,
+  publishVideo
+);
+router.route('/update/:videoId').patch(upload.none(), auth, updateVideo);
+router.route('/update-thumbnail/:videoId').patch(upload.single("thumbnail"), auth, updateThumbnail);
+router.route('/publish-status/:videoId').get(auth, togglePublishStatus);
+router.route('/delete/:videoId').delete(auth, deleteVideo);
+
 
 
 
